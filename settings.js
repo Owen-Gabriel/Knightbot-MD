436e41b94a5d1c00080bd6a78f2d63b30665701f
Owen Gabriel <97639957+Owen-Gabriel@users.noreply.github.com>
2025-04-01 23:14:54 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'OG',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: 'OG TECH', // Your name
+  ownerNumber: '2347067161378', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
